a95d2f69c3b66fc001523b5e7f505746a30ca80f
ɖʊʀɛʐ☆ <lorddurez@gmail.com>
2026-08-26 16:38:16 +0100

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,24 @@
+export const config = {
+    botName: "GOJO XMD",
+
+    owner: {
+        name: "Durez",
+        telegram: "@Durez_Not_durez",
+        country: "Nigeria",
+        email: "lorddurez@gmail.com"
+    },
+
+    prefix: ".",
+
+    sessionFolder: "./auth_info",
+
+    settings: {
+        welcome: true,
+        antiLink: false,
+        autoViewStatus: false,
+        autoReactStatus: false,
+        autoReactChat: false,
+        autoRecord: false,
+        autoType: false
+    }
+};
